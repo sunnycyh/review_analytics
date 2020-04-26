ed4586645aff5c5c1fa063b32957f65cc77fdb9f
Sunny Choi <yukhengchoi@gmail.com>
2020-04-27 02:21:10 +0800

    calc avg len of reviews

--- read1.py
+++ read1.py
@@ -7,7 +7,11 @@ with open('original.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
+print('Files ended, there are', len(data), 'in total.')
 
-print(data[0])
-print('----------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len += len(d) #sum_len = sum_len + len(d)
+	
+average = sum_len / len(data)
+print('The average numbers of words per reply is:', average)
